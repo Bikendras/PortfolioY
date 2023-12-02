fa3893cea1d64de15b20d31484ce35008c8f3f64
Bikendras <146335724+Bikendras@users.noreply.github.com>
2023-12-03 03:51:11 +0530

    first commit...

---
+++ src/Components/AboutContent.jsx
@@ -0,0 +1,25 @@
+import "./AboutContent.css";
+import {Link} from "react-router-dom";
+import React from 'react'
+
+export default function AboutContent() {
+  return (
+    <div className="about">
+        <div className="left">
+            <h1>Who Am I ?</h1>
+            <p> My name is Bikendra singh, it's a begning of my carrer in IT field .During my training session learn many languages to grow my self and also gain more Knowledge . With this i can create new more effective projects. </p>
+            <Link to="/contact">
+                <button className="btn">Contact</button>
+            </Link>
+        </div>
+
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack ">
+                    <img src="bikendra.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
